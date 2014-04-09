65fb16c2705680701653216f92bce0812d966829
beepal <b.paliak@gmail.com>
2014-04-09 18:49:24 +0530

    Google maps example nt working

---
+++ TestCarp/src/com/example/testcarp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.testcarp;
+
+import android.R;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_list_item);
+ }
+
+}
